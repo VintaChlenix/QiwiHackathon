7738712d3a2e18722b06a9a748a7155e5af2492b
Vinta <iluha.uchiha@mail.ru>
2023-07-20 17:08:43 +0300

    v0.1

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,76 @@
 package main
 
 import (
-	config2 "go.uber.org/zap/config"
-	"go.uber.org/zap/internal/app"
+	"bufio"
+	"encoding/xml"
+	"example.com/m/v2/internal/xmls"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
 )
 
+func getExchangeRate(rawCode, rawDate string) (string, error) {
+	_, code, _ := strings.Cut(rawCode, "=")
+	_, date, _ := strings.Cut(rawDate, "=")
+	splittedDate := strings.Split(date, "-")
+	url := fmt.Sprintf("http://www.cbr.ru/scripts/XML_daily.asp?date_req=%s/%s/%s", splittedDate[2], splittedDate[1], splittedDate[0])
+
+	valCurs, err := getValCursFromURL(url)
+	if err != nil {
+		return "", fmt.Errorf("failed to get exchange schema: %v", err)
+	}
+
+	for _, elem := range valCurs.Valute {
+		if elem.CharCode == code {
+			return elem.Value, nil
+		}
+	}
+
+	return "", fmt.Errorf("failed to find your currency")
+}
+
+func getValCursFromURL(url string) (*xmls.ValCurs, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get xml scheme: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status error: %v", response.StatusCode)
+	}
+
+	schema, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("read body: %v", err)
+	}
+
+	exchangeRates := new(xmls.ValCurs)
+	if err := xml.Unmarshal(schema, &exchangeRates); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal xml: %w", err)
+	}
+
+	return exchangeRates, nil
+}
+
 func main() {
-	config, err := config2.
-	exApp, err := app.NewApp()
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), " ")
+		switch data[0] {
+		case "currency_rates":
+			result, err := getExchangeRate(data[1], data[2])
+			if err != nil {
+				fmt.Printf("failed to get exchange rate: %v", err)
+				continue
+			}
+			fmt.Println(result)
+		case "close":
+			return
+		default:
+			fmt.Println("Wrong command!")
+		}
+	}
 }
